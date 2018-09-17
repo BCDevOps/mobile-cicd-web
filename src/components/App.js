4c9201ddb6c024cdb5d714a882017226e2124455
Jason C. Leach <jason.leach@fullboar.ca>
2018-09-17 15:37:28 -0700

    Code cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,13 @@ import FileUpload from './FileUpload';
 
 /* eslint-disable react/prefer-stateless-function */
 class App extends Component {
-  handleUploadFile = event => {
-    console.log('***************');
-    const data = new FormData();
-    data.append('file', event.target.files[0]);
-    data.append('name', 'some value user types');
-    data.append('description', 'some value user types');
-  };
+  // handleUploadFile = event => {
+  //   console.log('***************');
+  //   const data = new FormData();
+  //   data.append('file', event.target.files[0]);
+  //   data.append('name', 'some value user types');
+  //   data.append('description', 'some value user types');
+  // };
 
   render() {
     return (
@@ -27,9 +27,7 @@ class App extends Component {
         <p className="App-intro">
           <button
             onClick={() => {
-              console.log('4', this.props.files);
               this.props.createSigningJob(this.props.files);
-              // this.input.value = '';
             }}
           >
             Upload Document
