4c519565fb34d82b337ce0e709749d6241570754
Jason C. Leach <jason.leach@fullboar.ca>
2018-09-17 15:08:51 -0700

    Update verbiage

--- src/components/App.js
+++ src/components/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Secure Sign</h1>
         </header>
         <FileUpload />
         <p className="App-intro">
