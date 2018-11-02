170efbd66ecd8f56410a96c8f68b565936c15f57
ShellyXueHan <shellshell456@gmail.com>
2018-11-02 15:44:05 -0700

    use higher-order component to warp the toggle

--- src/components/Instruction.js
+++ src/components/Instruction.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Instruction.css';
 import { XML_SAMPLES } from '../constants';
+import Toggle from './Toggle';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dark } from 'react-syntax-highlighter/dist/styles/prism';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -14,21 +15,10 @@ class Instruction extends Component {
   toggledHandler = toggled => this.setState({ toggled });
 
   render() {
-    // For the state of instruction component
-    const toggleClass = this.state.toggled ? 'on' : '';
-    // For the state of button component
-    const toggleIcon = this.state.toggled ? (
-      <FontAwesomeIcon icon={faArrowUp} className="file-icon" onClick={() => {this.toggledHandler(false)}} /> 
-    ) : (
-      <FontAwesomeIcon icon={faArrowDown} className="file-icon" onClick={() => {this.toggledHandler(true)}} />
-    );
-
     return (
       <div className="instructions">
         <h3>Steps to Upload Your App</h3>
-        <article className={`instruction ${toggleClass}`}>
-          <div className="toggle"> {toggleIcon} </div>
-          <h4>XcodeArchive</h4>
+        <Toggle title="XcodeArchive">
           <p>To package up an xcarchive to submit for signing you need to:</p>
           <ol>
             <li>
@@ -46,7 +36,39 @@ class Instruction extends Component {
               </SyntaxHighlighter>
             </li>
           </ol>
-        </article>
+        </Toggle>
+        <Toggle title="IPA">
+          <p>To package up an xcarchive to submit for signing you need to:</p>
+          <ol>
+            <li>
+              <p>Create a folder to hold the xcarchive and options.plist</p>
+            </li>
+            <li>
+              <p>Copy the xcarchive from xcode into the folder from step 1</p>
+            </li>
+            <li>
+              <p>
+                Create or copy your options.plist from step 1, you could update the content from below:
+              </p>
+            </li>
+          </ol>
+        </Toggle>
+        <Toggle title="APK">
+          <p>To package up an xcarchive to submit for signing you need to:</p>
+          <ol>
+            <li>
+              <p>Create a folder to hold the xcarchive and options.plist</p>
+            </li>
+            <li>
+              <p>Copy the xcarchive from xcode into the folder from step 1</p>
+            </li>
+            <li>
+              <p>
+                Create or copy your options.plist from step 1, you could update the content from below:
+              </p>
+            </li>
+          </ol>
+        </Toggle>
       </div>
     );
   }
---
+++ src/components/Toggle.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './Toggle.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+
+class Toggle extends Component {
+  state = {
+    toggled: false,
+  };
+
+  toggledHandler = toggled => this.setState({ toggled });
+
+  render() {
+    const toggleClass = this.state.toggled ? 'on' : '';
+    const toggleIcon = this.state.toggled ? (
+      <FontAwesomeIcon icon={faArrowUp} className="toggle-icon" onClick={() => {this.toggledHandler(false)}} /> 
+    ) : (
+      <FontAwesomeIcon icon={faArrowDown} className="toggle-icon" onClick={() => {this.toggledHandler(true)}} />
+    );
+    const { children, title } = this.props;
+
+    return (
+      <div className={`instruction ${toggleClass}`}>
+        <div className="toggle-header">
+          <div className="toggle-title">{title}</div>
+          <div className="toggle-icon">{toggleIcon}</div>
+        </div>
+        <div>{children}</div>
+      </div>
+    );
+  }
+}
+
+export default Toggle;
