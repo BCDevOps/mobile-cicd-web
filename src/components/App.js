8359f174661c8721865b381a93ca4cd79f2d4975
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-11-19 14:36:41 -0800

    add twitter card

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { createSigningJob } from '../actionCreators';
 import { authenticateFailed, authenticateSuccess } from '../actions';
 import implicitAuthManager from '../auth';
 import './App.css';
+import Aux from '../hoc/auxillary';
 import FileUpload from './FileUpload';
 import Footer from './Footer';
 import Header from './Header';
@@ -38,63 +39,66 @@ export class App extends Component {
 
   render() {
     return (
-      <div>
-        <Header authentication={this.props.authentication} />
-        <div className="container">
-          {/* <form> */}
-          <ul className="flex-outer">
-            <li>
-              <label>Drag and drop the archive you with to sign onto this area.</label>
-              <FileUpload files={this.state.files || []} onFileAccepted={this.onFileAccepted} />
-            </li>
-            <li>
-              <p>What is the deployment platform this archive is meant for?</p>
-              <ul className="flex-inner">
-                <li>
-                  <input
-                    type="radio"
-                    id="platform-ios"
-                    name="platform"
-                    value="ios"
-                    onChange={this.onPlatformChanged}
-                  />
-                  <label htmlFor="platform-ios">iOS</label>
-                </li>
-                <li>
-                  <input
-                    type="radio"
-                    id="platform-android"
-                    name="platform"
-                    value="android"
-                    onChange={this.onPlatformChanged}
-                  />
-                  <label htmlFor="platform-android">Android</label>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <button
-                onClick={() => {
-                  if (!implicitAuthManager.isAuthenticated()) {
-                    alert('You need to login before you can submit signing jobs.');
-                    return;
-                  }
+      <Aux>
 
-                  this.props.createSigningJob(this.state.files, this.state.platform);
-                }}
-              >
-                Start
-              </button>
-            </li>
-          </ul>
-          {/* </form> */}
-          <JobStatusIndicator job={this.props.job} />
+        <div>
+          <Header authentication={this.props.authentication} />
+          <div className="container">
+            {/* <form> */}
+            <ul className="flex-outer">
+              <li>
+                <label>Drag and drop the archive you with to sign onto this area.</label>
+                <FileUpload files={this.state.files || []} onFileAccepted={this.onFileAccepted} />
+              </li>
+              <li>
+                <p>What is the deployment platform this archive is meant for?</p>
+                <ul className="flex-inner">
+                  <li>
+                    <input
+                      type="radio"
+                      id="platform-ios"
+                      name="platform"
+                      value="ios"
+                      onChange={this.onPlatformChanged}
+                    />
+                    <label htmlFor="platform-ios">iOS</label>
+                  </li>
+                  <li>
+                    <input
+                      type="radio"
+                      id="platform-android"
+                      name="platform"
+                      value="android"
+                      onChange={this.onPlatformChanged}
+                    />
+                    <label htmlFor="platform-android">Android</label>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <button
+                  onClick={() => {
+                    if (!implicitAuthManager.isAuthenticated()) {
+                      alert('You need to login before you can submit signing jobs.');
+                      return;
+                    }
+
+                    this.props.createSigningJob(this.state.files, this.state.platform);
+                  }}
+                >
+                  Start
+                </button>
+              </li>
+            </ul>
+            {/* </form> */}
+            <JobStatusIndicator job={this.props.job} />
+          </div>
+          <div className="container">
+            <Instruction />
+          </div>
+          <Footer />
         </div>
-        <div className="container">
-          <Instruction />
-        </div>
-        <Footer />
-      </div>
+      </Aux>
     );
   }
 }
---
+++ src/components/TwitterCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Aux from '../../hoc/auxillary';
+
+const TwitterCard = () => (
+  <Aux>
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="Mobile Signing" />
+    <meta
+      name="twitter:description"
+      content="Sign iOS or Android applications for public or enterprise distribution"
+    />
+    <meta name="twitter:image" content="" />
+  </Aux>
+);
+
+export default TwitterCard;
