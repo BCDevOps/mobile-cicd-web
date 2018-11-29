d3d4ddb87f26193104f76f5f550acdc1829782ef
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-11-29 15:52:31 -0800

    add homepage functional test

--- Functional-test/src/test/groovy/Specs/HomePageSpec.groovy
+++ Functional-test/src/test/groovy/Specs/HomePageSpec.groovy
@@ -5,23 +5,29 @@ import pages.*
 @Title("Signing Homepage")
 @Narrative("I can go to home page and login")
 @Stepwise
+@Ignore("Ignoring until there is a test IDIR account/password")
 class  A_HomePageSpec extends GebReportingSpec {
   def "I see the home page"(){
     when:"I go to the homepage"
     to HomePage
 
     then: "I am not logged in"
+    assert loginButton.text() == "LOGIN"
 
     and: "I click on login button"
+    loginButton.click()
 
     then: "I should be redirected to the login page"
-
+    at SSOPage
+    
     and: "I enter my credential and submit"
-    // Dotenv dotenv = Dotenv.configure().directory("./").load()
-    // IDIRusername = dotenv.get("IDIR_USERNAME")
-    // IDIRpassword = dotenv.get("IDIR_PASSWORD")
+
+    username.value(IDIRusername)
+    password.value(IDIRpassword)
+    submitButton.click()
 
     then: "I should be at homepage and logged in"
     at HomePage
+    assert loginButton.text() == "LOGOUT"
   }
 }
