73211231d44a3eba113419b99423fb2c72e26703
Jason C. Leach <jason.leach@fullboar.ca>
2020-03-24 11:27:17 -0700

    Remove IDMP hint

--- src/auth.js
+++ src/auth.js
@@ -27,7 +27,7 @@ const config = {
       : 'https://sso-dev.pathfinder.gov.bc.ca',
   realmName: 'devhub',
   clientId: 'signing-web',
-  kcIDPHint: 'idir', // Skip SSO, go directly to IDIR auth.
+  kcIDPHint: '', // Skip SSO, go directly to IDIR auth.
 };
 
 const iam = new ImplicitAuthManager(config);
